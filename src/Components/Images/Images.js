00ba83eaf5e3f00978d1af5d11175601432c96fd
emilio-matics <53688141+emilio-matics@users.noreply.github.com>
2020-04-15 16:35:20 +0300

    Images in progress

--- src/Components/Images/Images.js
+++ src/Components/Images/Images.js
@@ -3,7 +3,33 @@ import React from 'react'
 const Images = () => {
     return (
         <div className="main-images">
-            <div className="common-header"></div>
+            <div className="common-header">
+
+            </div>
+            <div className="common-title">
+                Images
+            </div>
+            <div className="main-images-container">
+                <div className="main-images-item">
+                    <img alt="" src={process.env.PUBLIC_URL + "/Images/dummy.jpg"}></img>
+                </div>
+                <div className="main-images-item">
+                    <img alt="" src={process.env.PUBLIC_URL + "/Images/dummy.jpg"}></img>
+                    <div className="main-images-item-completed">1</div>
+                </div>
+                <div className="main-images-item">
+                    <img alt="" src={process.env.PUBLIC_URL + "/Images/dummy.jpg"}></img>
+                </div>
+                <div className="main-images-item">
+                    <img alt="" src={process.env.PUBLIC_URL + "/Images/dummy.jpg"}></img>
+                </div>
+                <div className="main-images-item">
+                    <img alt="" src={process.env.PUBLIC_URL + "/Images/dummy.jpg"}></img>
+                </div>
+                <div className="main-images-item">
+                    <img alt="" src={process.env.PUBLIC_URL + "/Images/dummy.jpg"}></img>
+                </div>
+            </div>
         </div>
     )
 }
