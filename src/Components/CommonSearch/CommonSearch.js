937592988c6e2035f52231da4095a6e63d3d5f2a
emilio-matics <53688141+emilio-matics@users.noreply.github.com>
2020-04-21 15:14:47 +0300

    search complete

--- src/Components/CommonSearch/CommonSearch.js
+++ src/Components/CommonSearch/CommonSearch.js
@@ -3,7 +3,9 @@ import React from 'react'
 const CommonSearch = (props) => {
     return (
         <div className="common-search">
+            <img alt="" src={process.env.PUBLIC_URL + "Images/searchS.svg"}></img>
             <input type="text" placeholder="Search a label"></input>
+            <img className="common-search-delete" alt="" src={process.env.PUBLIC_URL + "Images/cancelS.svg"}></img>
         </div>
     )
 }
